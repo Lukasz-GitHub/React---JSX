707ac01065c169b3f6e38fe2aaf8c0cb5dba5b03
Lukasz Borek <borek_lukasz@outlook.com>
2018-07-22 22:58:16 +0200

    Initial Commit

---
+++ components/Contact.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var Contact = React.createClass({
+    propTypes: {
+        item: React.PropTypes.object.isRequired,
+    },
+
+    render: function () {
+        return (
+            <div className={'contactItem'}>
+                <img className={'contactImage'} src={'https://www.shubhbank.com/wp-content/uploads/2018/05/personal.png'} />
+                <p className={'contactLabel'}>
+                    Imię: {this.props.item.firstName}
+                </p>
+                <p className={'contactLabel'}>
+                    Nazwisko: {this.props.item.lastName}
+                </p>
+                <a className={'contactEmail'} href={'mailto: ' + this.props.item.email}>
+                    {this.props.item.email}
+                </a>
+            </div>
+        )
+    }
+});
